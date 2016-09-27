b0d149e34aff57d6de37e44d7ddd81474f72d1f4
suqinghe <373269349@qq.com>
2016-09-27 09:30:48 +0800

    源码更新

--- www/js/controllers/Controllers.js
+++ www/js/controllers/Controllers.js
@@ -11,17 +11,17 @@ angular.module('starter.controllers', [])
 			$ionicHistory.clearHistory();
 		});
 
- 
+
 
 		$scope.login = function() {
-			$http.post(HOST2 + "imitationShy/login.do", $scope.user, {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			}).success(function(response) {
+			alert(1);
+			var url = HOST2 + "imitationShy/login.do";
+			 
+			$http.post(url, $scope.user).success(function(response) {
+				alert(2);
 				if (response.success) {
 					$http.post(HOST2 + "imitationShy/customerinfo.do", {
 						"mobile": $scope.user.mobile
-					}, {
-						'Content-Type': 'application/x-www-form-urlencoded'
 					}).success(function(response) {
 						if (response.success) {
 							var item = angular.fromJson(response);
